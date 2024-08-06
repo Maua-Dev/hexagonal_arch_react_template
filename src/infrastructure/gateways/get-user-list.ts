1c3ab0ceeb8c8c06d4cde3a10560b87bbd5ce73d
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-08-05 23:40:15 -0300

    feat(gateway): add user gateways

---
+++ src/infrastructure/gateways/get-user-list.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpResponse } from '../http/http'
+import { User } from '../../domain/entities/user'
+
+interface GetUserList {
+  load(): Promise<HttpResponse<User[]>>
+}
+
+class GetUserListGateway implements GetUserList {
+  constructor(private readonly httpClient: HttpClient<HttpResponse<User[]>>) {}
+
+  async load(): Promise<HttpResponse<User[]>> {
+    return await this.httpClient.request({
+      method: 'get',
+      url: 'https://jsonplaceholder.typicode.com/users'
+    })
+  }
+}
+
+const httpClient: HttpClient<HttpResponse<User[]>> = {
+  async request(data) {
+    try {
+      const response = await fetch(data.url, {
+        method: data.method
+      })
+
+      if (response.status === 404) {
+        return {
+          statusCode: response.status,
+          data: []
+        }
+      }
+
+      if (!response.ok) {
+        throw new Error('Error fetching: ' + response.statusText)
+      }
+
+      const dataResponse = ((await response.json()) as User[]).map((user) =>
+        User.fromJson(user)
+      )
+
+      return {
+        statusCode: response.status,
+        data: dataResponse
+      }
+    } catch (error) {
+      throw new Error(
+        'Something went wrong on list users: ' + (error as Error).message
+      )
+    }
+  }
+}
+
+export const getUserListGateway = new GetUserListGateway(httpClient)
---
+++ src/infrastructure/gateways/get-user.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpResponse } from '../http/http'
+import { User } from '../../domain/entities/user'
+
+interface GetUser {
+  load(id: number): Promise<HttpResponse<User>>
+}
+
+class GetUserGateway implements GetUser {
+  constructor(private readonly httpClient: HttpClient<HttpResponse<User>>) {}
+
+  async load(id: number): Promise<HttpResponse<User>> {
+    return await this.httpClient.request({
+      method: 'get',
+      url: `https://jsonplaceholder.typicode.com/users/${id}`
+    })
+  }
+}
+
+const httpClient: HttpClient<HttpResponse<User>> = {
+  async request(data) {
+    try {
+      const response = await fetch(data.url, {
+        method: data.method
+      })
+
+      if (response.status === 404) {
+        return {
+          statusCode: response.status
+        }
+      }
+
+      if (!response.ok) {
+        throw new Error('Error fetching: ' + response.statusText)
+      }
+
+      const dataResponse = (await response.json()) as User
+
+      return {
+        statusCode: response.status,
+        data: dataResponse
+      }
+    } catch (error) {
+      throw new Error(
+        'Something went wrong on get user: ' + (error as Error).message
+      )
+    }
+  }
+}
+
+export const getUserGateway = new GetUserGateway(httpClient)
