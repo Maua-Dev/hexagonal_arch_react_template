3dd768d022b4b4b10cf8488c605a2a177dbc4e86
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-08-05 23:41:38 -0300

    feat(request): add basic request

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react'
+import { User } from '../domain/entities/user'
+import { userService } from '../services/user-service'
+
 export function App() {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const userList = await userService.listUsers()
+        setUsers(userList)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchUsers()
+  }, [])
   return (
     <>
-      <h1 className="flex flex-col text-9xl">OIE</h1>
+      <h1 className="flex flex-col text-9xl">React</h1>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </>
   )
 }
