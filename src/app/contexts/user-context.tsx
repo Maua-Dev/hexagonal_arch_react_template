1d5e0c1fea72381ef20adb042a44fe71a8c496aa
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-08-07 22:55:09 -0300

    feat: add user context

--- src/app/contexts/user-context.tsx
+++ src/app/contexts/user-context.tsx
@@ -4,15 +4,18 @@ import { User } from '../../domain/entities/user'
 type UserContextType = {
   users: User[]
   setUsers: React.Dispatch<React.SetStateAction<User[]>>
+  user: User
+  setUser: React.Dispatch<React.SetStateAction<User>>
 }
 
 export const UserContext = createContext<UserContextType | undefined>(undefined)
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [users, setUsers] = useState<User[]>([])
+  const [user, setUser] = useState<User>({} as User)
 
   return (
-    <UserContext.Provider value={{ users, setUsers }}>
+    <UserContext.Provider value={{ users, setUsers, user, setUser }}>
       {children}
     </UserContext.Provider>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app/app.tsx'
 import './app/styles/global.css'
+import { UserProvider } from './app/contexts/user-context.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 )
