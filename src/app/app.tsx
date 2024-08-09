9898b6b03cc3d7633ca27f8a540b207c988d2825
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-08-09 18:42:24 -0300

    chore: add router lib

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,25 +1,15 @@
-import { useEffect } from 'react'
-import { useUsers } from './hooks/use-users'
-import { UserCard } from './components/user-card'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/home'
 
 export function App() {
-  const { users, fetchUsers } = useUsers()
-
-  useEffect(() => {
-    fetchUsers()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
   return (
-    <main className="flex h-screen w-full flex-col items-center justify-center gap-12">
-      <h1 className="flex flex-col text-4xl font-semibold sm:text-6xl md:text-8xl">
-        Hexagonal Arch
-      </h1>
-      <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3">
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} />
-        ))}
-      </div>
-    </main>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </Router>
+    </>
   )
 }
